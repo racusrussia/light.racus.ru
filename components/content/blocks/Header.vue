e283e22f565471e9d5c086eb6028e34fd5b8e3f6
Yura <Yurgeman@users.noreply.github.com>
2022-03-03 18:40:18 +0300

    update Header.vue

--- components/content/blocks/Header.vue
+++ components/content/blocks/Header.vue
@@ -2,23 +2,23 @@
 
   <h2 v-if="this.data.data.level === 2"
       class="ra-content__heading"
-      v-text="data.data.text"></h2>
+      v-html="data.data.text"></h2>
 
   <h3 v-else-if="this.data.data.level === 3"
       class="ra-content__heading"
-      v-text="data.data.text"></h3>
+      v-html="data.data.text"></h3>
 
   <h4 v-else-if="this.data.data.level === 4"
       class="ra-content__heading"
-      v-text="data.data.text"></h4>
+      v-html="data.data.text"></h4>
 
   <h5 v-else-if="this.data.data.level === 5"
       class="ra-content__heading"
-      v-text="data.data.text"></h5>
+      v-html="data.data.text"></h5>
 
   <h6 v-else
       class="ra-content__heading"
-      v-text="data.data.text"></h6>
+      v-html="data.data.text"></h6>
 
 </template>
 
